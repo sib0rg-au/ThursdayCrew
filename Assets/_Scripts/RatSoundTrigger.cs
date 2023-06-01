9a7170b8cf8f8a44f99c3412b547da80374784a1
sib0rg-au <127904273+sib0rg-au@users.noreply.github.com>
2023-06-01 20:33:22 +0930

    Added rat sounds: trigger not working properly
    Added a script to trigger a rat sound in sewer. For some reason everything is triggering at the start? Fix next time

---
+++ Assets/_Scripts/RatSoundTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatSoundTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource m_AudioSource;
+    Collider m_Collider;
+    public GameObject player;
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+        m_Collider = GetComponent<Collider>();
+    }
+
+  
+    private void OnTriggerEnter(Collider col)
+    {
+       if (player.tag == "Player")
+        {
+            m_AudioSource.Play();
+            Debug.Log("Squeak");
+            //m_Collider.enabled = false;
+        }
+        
+    }
+}
